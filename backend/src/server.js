86a79544d2daeeae19fdfceb9b476592fcb6fe6d
gaurigoyal2020 <1000018487@dit.edu.in>
2026-02-22 23:27:32 +0530

    remove backend code from main

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-// src/server.js
-import 'dotenv/config';
-import app from './app.js';
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
