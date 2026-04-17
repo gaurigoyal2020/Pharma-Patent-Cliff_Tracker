5538f3e16f3b391fc280b0e24697a6754b169a31
drishti-cs1251 <dginfo1234@gmail.com>
2026-04-17 11:04:10 +0530

    Corrected import statement of eda css

--- src/pages/EdaPage.jsx
+++ src/pages/EdaPage.jsx
@@ -8,7 +8,7 @@ import {
 } from 'recharts';
 import Header from '../components/Layout/Header';
 import Footer from '../components/Layout/Footer';
-import './EDAPage.css';
+import './EdaPage.css';
 
 const FLASK_URL = import.meta.env.VITE_FLASK_URL || 'http://localhost:5001';
 
